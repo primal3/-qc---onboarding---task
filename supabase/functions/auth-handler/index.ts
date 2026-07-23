bfff34ad023613594cd950e4fc61ec9a2c5668ad
primal3 <phalakgodisho3@gmail.com>
2026-07-24 00:00:37 +0200

    working on auth-handler

--- supabase/functions/auth-handler/index.ts
+++ supabase/functions/auth-handler/index.ts
@@ -1,5 +1,7 @@
 import {serve} from "https://deno.land/std@0.177.0/http/server.ts";
 import {createClient} from "@supabase/supabase-js";
+import validator from "validator";
+import axios from "axios";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL") ?? "";
 const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY") ??"";
@@ -7,6 +9,9 @@ const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY") ??"";
 const supabase = createClient(supabaseUrl,supabaseAnonKey);
 //8KB
 const MAX_BODY_SIZE = 8*1024;
+const MINUTE = 60000;
+const MINUTES_10 = 10*MINUTE;
+const HOUR = 60*MINUTE;
 
 const corsHeaders = { 
 'Access-Control-Allow-Origin':  '*', 
@@ -32,7 +37,7 @@ try {
     }
 
     //Read and parse the JSON request body. 
-    const {action,first_name,last_name,email,phone,password} = await req.json();
+    const {action,first_name,last_name,email,phone,password,user_id} = await req.json();
     // check for missing fields
     if(!action){
         return new Response(
@@ -73,13 +78,30 @@ try {
         );
     }    
 
+    if(!user_id){
+        return new Response(
+            JSON.stringify(
+                {error:"user_id field is required"}
+            ),{
+                status:400,headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+    }    
+
+    //validate
+    validateNames(first_name);
+    validateNames(last_name);
+    validatePhone(phone);
+    validateEmail(email);
+    validatePassword(password);
+
     //Handle authentication
     switch(action){
 
         case"signup":
-           return await handleSignUp();
+           return await handleSignUp(first_name,last_name,email,password,phone);
         case "login":
-            return await handleLogIn();
+            return await handleLogIn(email,password,user_id);
         default:
             return new Response(
                 JSON.stringify({error:"Unknown action: ${action}"}),
@@ -112,14 +134,18 @@ async function handleSignUp(first_name: string,last_name: string,email:string,pa
      */
 
     try{
-        const {data,error} = await supabase.auth.signUp(
+        const {data,error} = await supabase.auth.admin.createUser(
             {
                 email,
                 password,
+                /**
+                 * 
                 options:{
                     emailRedirectTo:Deno.env.get("SIGNUP_REDIRECT"),
 
                 },
+                */
+
             }
         );
         if(error){
@@ -135,13 +161,16 @@ async function handleSignUp(first_name: string,last_name: string,email:string,pa
                 }
             );
         }
+        //add profile data
+        addProfile(data.user.id,first_name,last_name,phone );
+
         return new Response(
             JSON.stringify(
                 {
                     success:true,
                     message:"Signup successful.",
                     user:data.user,
-                    session:data.session,
+                    user_id:data.user.id,
                 }
             ),
             {
@@ -165,16 +194,493 @@ async function handleSignUp(first_name: string,last_name: string,email:string,pa
     }
 
     
-const { data, error } = await supabase
-  .from('profiles')
-  .insert([
-    { id: 'someValue', first_name: '${first_name}',last_name: '${last_name}',phone: '${phone}' },
-  ])
-  .select()
+
 
 }
-async function handleLogIn() {
+async function addProfile(user_id:string,first_name: string,last_name: string,phone: string ){
+    const { data, error } = await supabase
+    .from('profiles')
+    .insert([
+        {id:user_id , first_name: first_name,last_name: last_name,phone: phone },
+    ])
+    .select()
+
+        if(error){
+            return new Response(
+                JSON.stringify(
+                    {
+                        error: error.message
+                    }
+                ),
+                {
+                    status:400,
+                    headers:{...corsHeaders,"Content-Type":"application/json"}
+                }
+            );
+        }
+
+
+        return new Response(
+            JSON.stringify(
+                {
+                    success:true,
+                    message:"profile added successful.",
+                    data:data,
+
+                    
+                }
+            ),
+            {
+                status:200,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );    
+
+}
+async function handleLogIn(email:string,password:string,user_id:string) {
      /**
      * todo
      */
+    try {
+
+       // check for locked out account
+        isAccountLocked(email);
+
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+        });
+
+        if(error){
+            /**
+             * add lockout timing logic
+             */
+            
+
+
+            const ip = await getPublicIP();
+            addLogInAttempts(email,user_id,ip,false, Date.now());
+
+            return new Response(
+                JSON.stringify(
+                    {
+                        error: error?.message
+                    }
+                ),
+                {
+                    status:400,
+                    headers:{...corsHeaders,"Content-Type":"application/json"}
+                }
+            );
+        }
+
+
+        return new Response(
+            JSON.stringify(
+                {
+                    success:true,
+                    message:"Login successful.",
+                    user:data.user,
+                    session:data.session,
+                    
+                }
+            ),
+            {
+                status:200,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+
+    } catch(error){
+        console.error("Login error:",error);
+        return new Response(
+            JSON.stringify(
+                {
+                    error:"Login failed"
+                }
+            ),
+            {
+                status:500,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+    }
+
+
+}
+
+//get ip_address
+async function getPublicIP(){
+    try{
+      const response = await axios.get("https://api.ipify.org?format=json");
+      return response.data.ip;
+    }catch(error){
+        /**
+         * 
+         *         return new Response(
+            JSON.stringify(
+                {
+                    error: "Failed to fetch IP error: "+error
+                }
+            ),
+            {
+                status:400,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+
+
+         */
+        console.error("Failed to fetch IP error: ",error)
+    }
+}
+//validate functions
+function validateNames(name:string){
+    let message ="";
+    if(!name)message="Name is empty" 
+
+    if(!(/^[A-Za-z ]+$/.test(name) )) message = "only letters and space are allowed" ;
+
+    return new Response(
+    JSON.stringify(
+        {
+            error:message
+        }
+    ),
+    {
+        status:500,
+        headers:{...corsHeaders,"Content-Type":"application/json"}
+    }
+);
+        
+}
+
+function validatePhone(phone:string){
+    let message ="";
+    if(!phone)message="phone is empty" 
+
+    else if(!(/^[0-9 ]+$/.test(phone) )) message = "digits are allowed" ;
+    else if(phone[0] !=='0'){message="the first digit must be 0"}
+    return new Response(
+    JSON.stringify(
+        {
+            error:message
+        }
+    ),
+    {
+        status:500,
+        headers:{...corsHeaders,"Content-Type":"application/json"}
+    }
+);
+        
+}
+
+function  validateEmail(email:string){
+    let message ="";
+    if(!email)message="email is empty" 
+    else if(!validator.isEmail(email)) message = "email pattern invaild"
+    return new Response(
+    JSON.stringify(
+        {
+            error:message
+        }
+    ),
+    {
+        status:500,
+        headers:{...corsHeaders,"Content-Type":"application/json"}
+    }
+);
+
+}
+
+function  validatePassword(password:string){
+ if(!password)return new Response(
+    JSON.stringify(
+        {
+            error:"password is empty"
+        }
+    ),
+    {
+        status:500,
+        headers:{...corsHeaders,"Content-Type":"application/json"}
+    }
+);
+}
+async function isAccountLocked(email:string) {
+    
+        const { data: account_lockouts, error } = await supabase
+        .from('account_lockouts')
+        .select('email')
+        
+            if(error){
+                return new Response(
+                    JSON.stringify(
+                        {
+                            error: error.message
+                        }
+                    ),
+                    {
+                        status:400,
+                        headers:{...corsHeaders,"Content-Type":"application/json"}
+                    }
+                );
+            }        
+        if(account_lockouts){
+            if(account_lockouts?.length>0){
+            account_lockouts?.forEach((account,_index)=>{
+            if(account.email === email){
+
+                
+            validateAccountLockOut(email);
+
+            }
+            });
+        
+            }
+
+        }
+
+
+}
+async function validateAccountLockOut(email:string){
+
+        try{ 
+            const { data: account_lockouts, error } = await supabase
+            .from('account_lockouts')
+            .select('locked_until')
+            .eq("email",email)
+            if(error){
+                return new Response(
+                    JSON.stringify(
+                        {
+                            error: error.message
+                        }
+                    ),
+                    {
+                        status:400,
+                        headers:{...corsHeaders,"Content-Type":"application/json"}
+                    }
+                );
+            }
+            
+            account_lockouts?.forEach((account,_index)=>{
+            if(account.locked_until >Date.now()){
+                return new Response(
+                    JSON.stringify(
+                        {
+                            error: "Your account is temporarily locked. Please try again in ${math.floor((account.locked_until -Date.now()) /1000/60)} minutes. "
+                        }
+                    ),
+                    {
+                        status:400,
+                        headers:{...corsHeaders,"Content-Type":"application/json"}
+                    }
+                );
+
+            }
+            });
+
+
+        } catch(error){
+        console.error("acount_lockout validation error:",error);
+        return new Response(
+            JSON.stringify(
+                {
+                    error:"acount_lockout validation failed"
+                }
+            ),
+            {
+                status:500,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+
+        
+    }            
+
+}
+async function addLogInAttempts(email:string ,user_id:string,ip_address:string,success:boolean,attempted_at:number) {
+            
+   try {
+
+             const { data, error } = await supabase
+            .from('login_attempts')
+            .insert([
+                { id: user_id, email: email, ip_address: ip_address  , success: success, attempted_at:attempted_at },
+            ])
+            .select()
+
+            if(error){
+            return new Response(
+                JSON.stringify(
+                    {
+                        error: error?.message
+                    }
+                ),
+                {
+                    status:400,
+                    headers:{...corsHeaders,"Content-Type":"application/json"}
+                }
+            );    
+
+            }
+
+        return new Response(
+            JSON.stringify(
+                {
+                    success:true,
+                    message:"addLogInAttempts successful.",
+
+                    
+                }
+            ),
+            {
+                status:200,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+    
+   } catch (error) {
+
+        return new Response(
+            JSON.stringify(
+                {
+                    error:"adding  log attempts   failed"+error
+                }
+            ),
+            {
+                status:500,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );    
+
+   }
+
+
+}
+
+async function getLogInAttempts(email:string,user_id:string) {
+    
+    try {
+        
+    const { data: login_attempts, error } = await supabase
+    .from('login_attempts')
+    .select('attempted_at')
+    .eq('id',user_id)
+
+    if(error){
+    return new Response(
+        JSON.stringify(
+            {
+                error: error?.message
+            }
+        ),
+        {
+            status:400,
+            headers:{...corsHeaders,"Content-Type":"application/json"}
+        }
+    );    
+
+    }
+
+
+    getLastLockType(user_id);
+    if(login_attempts){
+    if(login_attempts?.length ===3){
+        if(login_attempts[-1].attempted_at < MINUTES_10 && login_attempts[-1].attempted_at < MINUTES_10 && login_attempts[-1].attempted_at < MINUTES_10)
+         
+         addAccountLocked(email,user_id,Date.now()+MINUTES_10,"short");            
+        }
+    else if(){
+
+    }
+    }
+
+
+    } catch (error) {
+        
+        return new Response(
+            JSON.stringify(
+                {
+                    error:"getting  log attempts   failed"+error
+                }
+            ),
+            {
+                status:500,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+
+    }
+
+
+
+
+}
+
+async function addAccountLocked( email:string,user_id:string,locked_until: number ,lockout_type:string) {
+try {
+
+    const { data, error } = await supabase
+    .from('account_lockouts')
+    .insert([
+        { id: user_id, email: email,locked_until:locked_until ,lockout_type:lockout_type},
+    ])
+    .select()
+
+
+    if(error){
+    return new Response(
+        JSON.stringify(
+            {
+                error: error?.message
+            }
+        ),
+        {
+            status:400,
+            headers:{...corsHeaders,"Content-Type":"application/json"}
+        }
+    );    
+
+    }    
+
+    
+} catch (error) {
+    
+        return new Response(
+            JSON.stringify(
+                {
+                    error:"adding  account locked   failed"+error
+                }
+            ),
+            {
+                status:500,
+                headers:{...corsHeaders,"Content-Type":"application/json"}
+            }
+        );
+
+
+}        
+
+
+}
+
+async function getLastLockType(user_id:string)  {
+  try {
+    
+    const { data: account_lockouts, error } = await supabase
+    .from('account_lockouts')
+    .select('lockout_type')
+    .eq('id',user_id)
+    
+    if(account_lockouts){
+        return account_lockouts[-1].lockout_type
+    }
+
+    
+  } catch (error) {
+    
+  }
+
 }
